0fd218c273c94f9358b38f56ee0211f72e41ee9e
Sri-Anu <54557955+Sri-Anu@users.noreply.github.com>
2020-09-16 20:48:21 -0400

    Update main.py

--- Fashion_MNIST with PyTorch/main.py
+++ Fashion_MNIST with PyTorch/main.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from torchsummary import summary
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -36,37 +37,32 @@ test_loader = torch.utils.data.DataLoader(test_set,
 class Fashion_MNIST(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3)
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        self.conv3 = nn.Conv2d(64, 128, 3)
-        self.dropout = nn.Dropout(p=0.2)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.MaxPool2d((2,2))
+        )
 
-        self._to_linear = None
-        x = torch.randn(28, 28).view(-1, 1, 28, 28)
-        self.conv(x)
-
-        self.fc1 = nn.Linear(self._to_linear, 512)
-        self.fc2 = nn.Linear(512, 10)
-    
-    def conv(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
-        x = F.dropout(x)
-        x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
-        x = self.dropout(x)
-        x = F.max_pool2d(F.relu(self.conv3(x)), (2,2))
-        x = self.dropout(x)
-
-        if self._to_linear is None:
-            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
-        return x
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.MaxPool2d((2,2))
+        )
+        
+        self.fc1 = nn.Linear(1600, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = self.conv(x)
-        x = x.view(-1, self._to_linear)
-        x = F.relu(self.fc1(x))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc1(x)
+        x = F.relu(x)
         x = self.fc2(x)
-        x = F.softmax(x, dim=1)
-        return x
+        output = F.log_softmax(x, dim=1)
+        return output
 
 NN = Fashion_MNIST()
 
